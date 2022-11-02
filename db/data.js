6b94fd17fb21271177a8da358e488c792c5d72d3
MartinGoesToSauerland <98959955+MartinGoesToSauerland@users.noreply.github.com>
2022-11-02 23:31:29 +0100

    dummy data

---
+++ db/data.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {id: 1, area_id: 1, status: 0, details: {}},
+    {id: 2, area_id: 1, status: 1, details: {
+        name: "Aaron",
+        created_at: "2022-10-31",
+        start_at: "",
+        end_at: ""
+    }},    
+    {id: 3, area_id: 1, status: 2, details: {
+        name: "Alexandra",
+        created_at: "2022-02-01",
+        start_at: "2022-02-02",
+        end_at: ""
+    }},        
+    {id: 4, area_id: 1, status: 0, details: {}},
+    {id: 5, area_id: 1, status: 0, details: {}},
+    {id: 6, area_id: 1, status: 0, details: {}},
+    {id: 7, area_id: 1, status: 0, details: {}},
+    {id: 8, area_id: 1, status: 0, details: {}},
+    {id: 9, area_id: 1, status: 0, details: {}},
+    {id: 10, area_id: 1, status: 0, details: {}},
+    {id: 11, area_id: 1, status: 0, details: {}},
+    {id: 12, area_id: 1, status: 0, details: {}},
+    {id: 13, area_id: 1, status: 0, details: {}},                                        
+]
