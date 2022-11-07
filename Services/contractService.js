898544149d7434b1622d7124824aaeaad8662c57
MartinGoesToSauerland <98959955+MartinGoesToSauerland@users.noreply.github.com>
2022-11-07 20:58:10 +0100

    Add contract changeStatus fn to service

--- Services/contractService.js
+++ Services/contractService.js
@@ -20,7 +20,7 @@ module.exports = class ContractService extends Service {
         const parcelService = new ParcelService;
 
         let userCheck = await userService.getByKey("email", data.user_data.email);
-        
+
         if (! userCheck) {
             // create User & fetch User
             const user_inserted_id = await userService.create(data.user_data);
@@ -33,7 +33,7 @@ module.exports = class ContractService extends Service {
         // Check if contract exists for this user and this parcel id which is active
         // then return false with message. allready reserved. please wait.
         let res = await this.Repository.getByKeys({
-            keys: ["user_id", "parcel_id"], 
+            keys: ["user_id", "parcel_id"],
             values: [userCheck.id, data.parcel_id]
         });
         if (res) {
@@ -41,7 +41,7 @@ module.exports = class ContractService extends Service {
         }
         //console.log(userCheck, data.user_data.email)
         //return userCheck;
-        
+
         // build contract Model
         const contractObject = {
             user_id: userCheck.id,
@@ -51,8 +51,8 @@ module.exports = class ContractService extends Service {
 
         // store Contract Model
         const model = new this.Model(contractObject);
-        const inserted_id = await this.Repository.create(model.object());  
-        
+        const inserted_id = await this.Repository.create(model.object());
+
 
         // get Parcel Object
         const parcel = await parcelService.getByKey("id", data.parcel_id);
@@ -60,7 +60,7 @@ module.exports = class ContractService extends Service {
         const newParcelData = {
             status: "reserved"
         }
-        
+
         //parcel.updated_at = new Date().toISOString();
         const parcel_inserted_id = await parcelService.updateAfterEdit(parcel, newParcelData);
         // send Email to WMAgrar
@@ -68,7 +68,50 @@ module.exports = class ContractService extends Service {
         emailService.sendEmail({data: this.flatObject(d.data)});
         // successdull response
         return parcel_inserted_id;
-    }    
+    }
+
+    async changeStatus(id, status) {
+        const parcelService = new ParcelService;
+        const allowedStatus = ["start", "end"];
+        const contractObject = {
+            params: '',
+            body: ''
+        }
+        contractObject.params = {
+            id: id
+        };
+
+        if (! allowedStatus.includes(status)) {
+            return false;
+        }
+        if (status === "start") {
+            const today = new Date();
+            contractObject.body = {
+                active: 1,
+                start_at: today.toISOString(),
+                end_at: this.addOneYear(today).toISOString()
+            };
+        } else {
+            contractObject.body = {
+                active: 0,
+            };
+        }
+
+        let contractCheck = await this.getByKey("parcel_id", id);
+
+        const model = new this.Model(contractCheck);
+
+        await this.Repository.update(contractCheck, contractObject.body);
+
+        // get Parcel Object
+        const parcel = await parcelService.getByKey("id", contractCheck.parcel_id);
+        // update Parcel Object
+        const newParcelData = {
+            status: "rented"
+        }
+        await parcelService.updateAfterEdit(parcel, newParcelData);
+        return true;
+    }
 
     flatObject(nestyObject) {
         console.log("C", nestyObject)
@@ -77,9 +120,15 @@ module.exports = class ContractService extends Service {
           if (typeof nestyObject[key] == "object") {
             flatedObject = {...flatedObject,...nestyObject[key]};
           } else {
-              flatedObject[key] = nestyObject[key]; 
+              flatedObject[key] = nestyObject[key];
           }
-        })        
+        })
         return flatedObject;
+    }
+
+    addOneYear(date) {
+        const clone = new Date(date);
+        clone.setFullYear(clone.getFullYear() + 1);
+        return clone;
     }
 }
