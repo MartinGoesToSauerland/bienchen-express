2a3edb5c054855bb3dc04e0d64faa84a6f39e081
MartinGoesToSauerland <98959955+MartinGoesToSauerland@users.noreply.github.com>
2022-11-07 09:21:31 +0100

    Rewrite repository fn for get parcels

--- Repositories/parcelRepository.js
+++ Repositories/parcelRepository.js
@@ -4,20 +4,57 @@ module.exports = class ParcelRepository extends Repository {
 
     table = 'parcels'
 
-    getAllWithRelationsByFilter(key, value) {
-        const sql = `SELECT p.id, p.status, c.user_id, c.start_at, c.end_at, c.created_at, u.firstname, u.lastname  FROM ${this.table} p 
-        LEFT JOIN  contracts  c ON c.parcel_id = p.id 
-        LEFT JOIN users u ON c.user_id = u.id 
-        WHERE p.${key} = ? AND c.active IS NOT 0`;
+    async getAllWithRelationsByFilter(key, value) {
+        // get all parcels
+        const sql = `SELECT p.id, p.status FROM ${this.table} p WHERE p.${key} = ? LIMIT 10`;
         const params = [value];
+        const parcels = await this.all(sql, params);
 
-        return this.all(sql, params);
+        // fetch all parcels ids which are reserved and rented
+        const ids = [];
+        parcels.forEach(_ => {
+          if (_.status != 0) {
+          ids.push(_.id)
+          }
+        });
+
+        // get all contracts with users relation
+        const sql2 = `SELECT c.active, c.parcel_id, c.user_id, c.start_at, c.end_at, c.created_at, u.id as user_id, u.firstname, u.lastname FROM contracts c
+        JOIN users u ON u.id = c.user_id
+        WHERE parcel_id IN (${ids}) AND active = 1`
+        const params2 = [];
+        const contracts = await this.all(sql2, params2);
+
+        // merge parcels with contracts
+        const dict = contracts.reduce((acc, val) => ({...acc, [val.parcel_id]: val}), {});
+
+        const result = parcels.map(val=> {
+          if (! dict[val.id]) return val; //If nothing in "dict" object by id we return original obj
+          const mergedObj = {...val, ...dict[val.id]}; //Merge two objects
+          delete Object.assign(mergedObj, {user_id: mergedObj.parcel_id})['parcel_id']; //create "user_id" by the value of "a_id"  and delete old key "a_id"
+          return mergedObj;
+        });
+
+        return result;
     }
 
     updateAfterEdit(object, newData) {
         return this.update(object, newData);
     }
     /*
+    async x_getAllWithRelationsByFilter(key, value) {
+        console.log("hello", key, value)
+        const sql = `SELECT p.id, p.status, c.active, c.user_id, c.start_at, c.end_at, c.created_at, u.id as user_id, u.firstname, u.lastname  FROM ${this.table} p
+        LEFT JOIN  contracts  c ON c.parcel_id = p.id
+        LEFT JOIN users u ON c.user_id = u.id
+        WHERE p.${key} = ? LIMIT 10`; // AND c.active IS NOT 0
+        const params = [value];
+
+        const data = await this.all(sql, params);
+        console.log(data)
+        return data.filter(_ => _.active != 0);
+    }
+        
     createCountry(data) {
         const sql = `INSERT INTO ${this.table} (code, name) VALUES (?,?)`;
         const params = [data.code, data.name];
@@ -30,12 +67,12 @@ module.exports = class ParcelRepository extends Repository {
         Object.keys(newCountryData).forEach(e => {
             keyValues.push(`${e} = ?`)
         });
-        
+
         const sql = `UPDATE ${this.table} SET ${keyValues.toString()} WHERE id = ?`;
         const params = [...Object.values(newCountryData), country.id];
 
         return this.run(sql, params);
-    }    
+    }
 
     getAllCountries() {
         const sql = `SELECT * FROM ${this.table}`;
@@ -55,7 +92,7 @@ module.exports = class ParcelRepository extends Repository {
         const sql = `SELECT * FROM ${this.table} WHERE ${key} = ?`;
         const params = [value];
         return this.get(sql, params);
-    }       
+    }
 
     deleteCountry(id) {
         const sql = `DELETE FROM ${this.table} WHERE id = ?`;
